bf767e22bd7a88a0d2144056d99e5cd49338aeb2
Josh Owens <me@jowens.dev>
2021-09-06 21:21:19 -0500

    Abstracting out scraping process into a separate function that deps can be injected into

--- scrapper/flaskr/scrape.py
+++ scrapper/flaskr/scrape.py
@@ -3,6 +3,19 @@ from recipe_scrapers import scrape_me
 
 bp = Blueprint('scrape', __name__, url_prefix='/scrape')
 
+def scrape_url(scraper, url):
+    recipe = scraper(url)
+
+    return {
+        'title': recipe.title(),
+        'time': recipe.total_time(),
+        'servings': recipe.yields(),
+        'ingredients': recipe.ingredients(),
+        'instructions': recipe.instructions().splitlines(),
+        'image': recipe.image(),
+        'url': url,
+    }
+
 @bp.route('/', methods=['POST'])
 def scrape():
-    return  scrape_me(request.form['url']).instructions() 
+    return scrape_url(scraper=scrape_me, url=request.form['url']) 
