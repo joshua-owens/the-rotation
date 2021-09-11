a3f712f76be4cba1826b8cd6103da36ba55fba54
Josh Owens <me@jowens.dev>
2021-09-11 16:55:55 -0500

    Updating types for Recipe abstract

--- api/app/Recipe.php
+++ api/app/Recipe.php
@@ -2,12 +2,23 @@
 
 namespace App;
 
+use Illuminate\Support\Collection;
+
+/**
+ * @property-read string $title
+ * @property-read string $time
+ * @property-read string $servings
+ * @property-read Collection $ingredients
+ * @property-read Collection instructions
+ * @property-read string $image
+ * @property-read string $url
+ */
 abstract class Recipe {
     abstract public function title(): string;
-    abstract public function time(): string;
+    abstract public function time(): int;
     abstract public function servings(): string;
-    abstract public function ingredients(): string;
-    abstract public function instructions(): string;
+    abstract public function ingredients(): Collection;
+    abstract public function instructions(): Collection;
     abstract public function image(): string;
     abstract public function url(): string;
 
