be8ce6f13bb918b8144ae329914118358fe5c0b0
Josh Owens <me@jowens.dev>
2022-05-25 22:33:49 -0500

    cli config options

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+type config struct {
+	port int
+	env  string
+}
+
 func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 3000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -16,12 +33,13 @@ func main() {
 	})
 
 	srv := http.Server{
-		Addr:         ":3000",
+		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      r,
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
+	logger.Printf("starting %s server on port %s", cfg.env, srv.Addr)
 	srv.ListenAndServe()
 }
