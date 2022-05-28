af9a6992b51f6a9937b6dc3573287fd9bdbf6aaa
Josh Owens <me@jowens.dev>
2022-05-27 20:27:52 -0500

    json response helper

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,7 +12,10 @@ func (app *application) routes() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
+		data := map[string]string{
+			"hello": "world",
+		}
+		app.json(w, http.StatusOK, data, nil)
 	})
 
 	return r
