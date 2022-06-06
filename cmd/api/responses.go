aa687e600ae82d194e7176539aa1e355f752caf9
Josh Owens <me@jowens.dev>
2022-06-05 20:39:38 -0500

    adding error helpers

--- cmd/api/responses.go
+++ cmd/api/responses.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
+
+type responseWrapper map[string]interface{}
+
+func (app *application) logError(r *http.Request, err error) {
+	app.logger.Println(err)
+}
 
 func (app *application) json(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
 	js, err := json.Marshal(data)
@@ -23,4 +30,26 @@ func (app *application) json(w http.ResponseWriter, status int, data interface{}
 	w.Write(js)
 
 	return nil
+}
+
+func (app *application) error(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
+
+	rw := responseWrapper{"error": message}
+
+	err := app.json(w, status, rw, nil)
+
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(500)
+	}
+}
+
+func (app *application) notFound(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("Not Found", r.Method)
+	app.error(w, r, http.StatusNotFound, message)
+}
+
+func (app *application) methodNotAllowed(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("%s Method Not Allowed", r.Method)
+	app.error(w, r, http.StatusMethodNotAllowed, message)
 }
