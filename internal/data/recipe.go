a5c5a1ab571fd8fa8b67bf8c2d24cc3ffa648502
Josh Owens <me@jowens.dev>
2022-06-06 21:14:02 -0500

    adding struct tag names for recipe

--- internal/data/recipe.go
+++ internal/data/recipe.go
@@ -1,11 +1,16 @@
 package data
 
+import "time"
+
 type Recipe struct {
-	Title        string
-	Time         int32
-	Servings     int32
-	Ingredients  []string
-	Instructions []string
-	Image        string
-	Url          string
+	ID           int64     `json:"id"`
+	Title        string    `json:"title"`
+	Time         int32     `json:"time"`
+	Servings     int32     `json:"servings"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	Image        string    `json:"image"`
+	Url          string    `json:"url"`
+	CreatedAt    time.Time `json:"-"`
+	UpdatedAt    time.Time `json:"-"`
 }
